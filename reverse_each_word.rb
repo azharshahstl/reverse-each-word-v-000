57604a8ff117a69c67f4dcd57d92460ade9c2631
Azhar Shah <azharshahstl@yahoo.com>
2018-07-16 13:35:55 +0000

    automatically backed up by learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+def reverse_each_word(words)
+  reverse_words = []
+  words = words.split(" ")
+  words.each do |word|
+    reverse_words << word.reverse
+    reverse_words.
+  end
+  reverse_words
+end
