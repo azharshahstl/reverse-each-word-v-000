db0ca9dad2ee86b6766efea475483e3bbddf9337
Azhar Shah <azharshahstl@yahoo.com>
2018-07-16 15:09:01 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,7 +7,7 @@ def reverse_each_word(words)
   reverse_words.join(" ")
 end
 
-def reverse_each_word (words)
+def reverse_each_word(words)
   reverse_words = []
   words = words.split(" ")
   words.collect do |word|
